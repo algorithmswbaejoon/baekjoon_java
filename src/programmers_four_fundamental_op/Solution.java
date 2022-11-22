8eb3a93ac9d076a45744b7dc2deb79bffce92ce8
kihwankim <clack2933@gmail.com>
2022-11-22 22:08:49 +0900

    feat: 사칙연산 문제 dp 문제 풀이 추가

---
+++ src/programmers_four_fundamental_op/Solution.java
@@ -0,0 +1,45 @@
+package programmers_four_fundamental_op;
+
+public class Solution {
+    public int solution(String[] arr) {
+        // size
+        int size = arr.length / 2 + 1;
+        int opCount = arr.length / 2 + 1;
+
+        // create dp
+        int[][] maxDp = new int[size][size];
+        int[][] minDp = new int[size][size];
+
+        // set init value
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                maxDp[i][j] = Integer.MIN_VALUE;
+                minDp[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        // init
+        for (int i = 0; i < size; i++) {
+            int intVar = Integer.parseInt(arr[i * 2]);
+            maxDp[i][i] = intVar;
+            minDp[i][i] = intVar;
+        }
+
+        for (int middleOpertationIndex = 1; middleOpertationIndex < opCount; middleOpertationIndex++) {
+            for (int frontStart = 0; frontStart < opCount - middleOpertationIndex; frontStart++) {
+                int postEnd = frontStart + middleOpertationIndex;
+                for (int frontEnd = frontStart; frontEnd < postEnd; frontEnd++) {
+                    if (arr[frontEnd * 2 + 1].equals("+")) {
+                        maxDp[frontStart][postEnd] = Math.max(maxDp[frontStart][postEnd], maxDp[frontStart][frontEnd] + maxDp[frontEnd + 1][postEnd]);
+                        minDp[frontStart][postEnd] = Math.min(minDp[frontStart][postEnd], minDp[frontStart][frontEnd] + minDp[frontEnd + 1][postEnd]);
+                    } else {
+                        maxDp[frontStart][postEnd] = Math.max(maxDp[frontStart][postEnd], maxDp[frontStart][frontEnd] - minDp[frontEnd + 1][postEnd]);
+                        minDp[frontStart][postEnd] = Math.min(minDp[frontStart][postEnd], minDp[frontStart][frontEnd] - maxDp[frontEnd + 1][postEnd]);
+                    }
+                }
+            }
+        }
+
+        return maxDp[0][opCount - 1];
+    }
+}
